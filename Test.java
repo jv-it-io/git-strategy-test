63defa4bf844b117054f5487f3388f446c7fafe4
jnn <jonathan.vandersmissen@fagg-afmps.be>
2023-08-04 09:31:44 +0200

    add getter setter

--- Test.java
+++ Test.java
@@ -1,4 +1,12 @@
 public class Test {
 
     private String testName;
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
 }
